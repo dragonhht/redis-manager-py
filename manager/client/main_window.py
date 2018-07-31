0abbf47ce2fdf75e861bea5a028d865ae9c1d53f
dragonhht <dragon_hht@163.com>
2018-07-31 11:44:13 +0800

    窗口基本功能

--- manager/client/main_window.py
+++ manager/client/main_window.py
@@ -1,20 +1,53 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*- 
 
-from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 
-def main():
-    # 创建应用
-    app = QApplication(sys.argv)
-    # 创建一个窗口
-    w = QWidget()
-    w.resize(500, 150)
-    w.move(100, 100)
-    w.setWindowTitle("Redis Manager")
-    w.show()
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
-    sys.exit(app.exec_())
+class ManagerWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+
+    def init_UI(self):
+        '''
+        初始化UI
+        '''
+        self.resize(500, 300)
+        self.center()
+        self.setWindowTitle("Redis Manager")
+        #设置icon
+        # w.setWindowIcon(QIcon('./icon/redis.png'))
+        self.show()
+
+    def closeEvent(self, event):
+        '''
+        关闭窗口提示
+        '''
+        replay = QMessageBox.question(self, '提示', '确定退出！！！', QMessageBox.Yes | QMessageBox.No,QMessageBox.No)
+        if replay == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+        '''
+        将窗口放在屏幕中
+        '''
+        # 获得这个QT组件的框架位置
+        qr = self.frameGeometry()
+        # 找到屏幕的中心
+        cp = QDesktopWidget().availableGeometry().center()
+        # 将这个矩形的中心放在当前这个屏幕的中心
+        qr.moveCenter(cp)
+        # 将创建的窗口移动
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    win = ManagerWindow()
+    sys.exit(app.exec_())
